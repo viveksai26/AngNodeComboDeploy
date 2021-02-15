ce496aee3c6d2c23f4c5dda055a4e992bd466882
Vivek <you@example.com>
2021-02-15 22:25:41 +0530

    base structure"

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,8 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './routes/app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { ErrorModule } from './modules/core/errors/error.module';
+import { SharedModule } from './modules/shared/shared.module';
+import { PublicModule } from './modules/feature/public/public.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +17,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    ReactiveFormsModule,
+    FormsModule,
+    CommonModule,
+    RouterModule,
+    HttpClientModule,
+    ErrorModule,
+    AppRoutingModule,
+    PublicModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
