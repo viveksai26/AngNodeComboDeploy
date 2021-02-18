6104d0c18ee6d6ae12f92de831aa856d7538a67d
Vivek <you@example.com>
2021-02-18 16:44:33 +0530

    eslint

--- client/src/app/modules/core/errors/error-component/error-component.component.spec.ts
+++ client/src/app/modules/core/errors/error-component/error-component.component.spec.ts
@@ -1,6 +1,6 @@
-import {ComponentFixture, TestBed} from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import {ErrorComponentComponent} from './error-component.component';
+import { ErrorComponentComponent } from './error-component.component';
 
 describe('ErrorComponentComponent', () => {
   let component: ErrorComponentComponent;
