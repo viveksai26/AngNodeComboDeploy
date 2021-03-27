9b9167af2994a114ca5cac2a3f5875fc2587156f
Vivek <you@example.com>
2021-03-27 09:49:11 +0530

    fix

--- client/src/app/modules/core/guards/authentication.guard.ts
+++ client/src/app/modules/core/guards/authentication.guard.ts
@@ -1,17 +1,14 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
-import { GoogleLoginProvider, SocialAuthService } from 'angularx-social-login';
+import { SocialAuthService } from 'angularx-social-login';
 import { Observable, of } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
-import { AppConstants } from '../../shared/constants/app-constants';
 import { RoutePathConstant } from '../../shared/constants/route-path-constants';
-import { AutheticationService } from '../../shared/services/authentication/authetication.service';
-import { NotificationService } from '../services/notification.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuardService implements CanActivate {
+export class AuthenticationGuardService implements CanActivate {
   constructor(private router: Router, private authService: SocialAuthService) {}
 
   canActivate(): Observable<any> {
--- client/src/app/modules/feature/secure/user/user-routing.module.ts
+++ client/src/app/modules/feature/secure/user/user-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AutoLoginService } from '../../../core/guards/auto-login.service';
-// import { AuthGuardService } from '../../../core/guards/auth.guard';
+import { AuthenticationGuardService } from '../../../core/guards/authentication.guard';
 import { SecureLayoutComponent } from '../../../shared/layout/secure/secure-layout/secure-layout.component';
 import { UserLayoutComponent } from './user-layout/user-layout.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
@@ -13,7 +13,7 @@ const routes: Routes = [
       {
         path: '',
         component: UserLayoutComponent,
-        canActivate: [AutoLoginService],
+        canActivate: [AuthenticationGuardService, AutoLoginService],
         children: [{ path: 'profile', component: UserProfileComponent }]
       }
     ]
