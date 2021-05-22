9114bb3b83b1b1b3a8268272993c0fb871ed6f11
Saivivek Rasala <viveksai26@gmail.com>
2021-05-22 20:08:47 +0530

    Public pages logout

--- client/src/app/modules/feature/public/login/login-routing.module.ts
+++ client/src/app/modules/feature/public/login/login-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { AutoLoginService } from '../../../core/guards/auto-login.service';
 import { PublicLayoutComponent } from '../../../shared/layout/public/public-layout/public-layout.component';
 import { CommonRouteResolverService } from '../../../shared/resolvers/common-route-resolver.service';
+import { PublicRouteResolverService } from '../../../shared/resolvers/public-route-resolver.service';
 import { LoginComponent } from './login.component';
 
 const routes: Routes = [
@@ -10,6 +11,7 @@ const routes: Routes = [
     path: '',
     canActivate: [AutoLoginService],
     component: PublicLayoutComponent,
+    resolve: [PublicRouteResolverService],
     children: [{ path: '', component: LoginComponent }]
   }
 ];
