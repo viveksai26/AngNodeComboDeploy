8407936bb742d52e9434dac82e2f5f9447bb31aa
viveksai26 <viveksai26@gmail.com>
2022-08-08 13:10:17 +0530

    Route fix

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 /* eslint-disable no-undef */
-//Install express server
+// Install express server
 const express = require('express');
 const path = require('path');
-const cors = require('cors')
+const cors = require('cors');
 var app = express();
-app.use(cors())
+app.use(cors());
 /**
  * Routes
  */
@@ -20,19 +20,15 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.get('/*', function(req,res) {
-//     res.send('Hello');
-// // res.sendFile(path.join(__dirname+'/dist/common-client/index.html'));
-// });
 
 app.use('/api', indexRouter);
 // catch 404 and forward to error handler
-app.use('/api', function (req, res, next) {
-  next(createError(404));
+app.use('/api', (req, res, next) => {
+  next(createError('Route not found', 404));
 });
 
 // error handler
-app.use('/api', function (err, req, res, next) {
+app.use('/api', (err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -42,30 +38,13 @@ app.use('/api', function (err, req, res, next) {
   res.render('error');
 });
 // Serve only the static files form the dist directory
-app.use('/common-client', express.static(__dirname + '/dist/common-client'));
-app.get('/common-client/*', function (req, res) {
-  res.sendFile(path.join(__dirname+'/dist/common-client/index.html'));
+app.use('/common-client', express.static(`${__dirname}/dist/common-client`));
+app.get('/common-client/*', (req, res) => {
+  res.sendFile(path.join(`${__dirname  }/dist/common-client/index.html`));
 });
-app.use('/', function (req, res) {
+app.use('/', (req, res, next) => {
   res.redirect('/common-client');
 });
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
-
-// //Install express server
-// const express = require('express');
-// const path = require('path');
-
-// const app = express();
-
-// // Serve only the static files form the dist directory
-// app.use(express.static(__dirname + '/dist/common-client'));
-
-// app.get('/*', function(req,res) {
-//     res.send('Hello');
-// // res.sendFile(path.join(__dirname+'/dist/common-client/index.html'));
-// });
-
-// // Start the app by listening on the default Heroku port
-// app.listen(process.env.PORT || 8080);
